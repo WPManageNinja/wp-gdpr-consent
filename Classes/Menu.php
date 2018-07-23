24ec71ea8dc3b34df6136764e28829c397af9ad7
Ruhel <ruhel241@gmail.com>
2018-07-23 13:48:07 +0600

    first commit

---
+++ Classes/Menu.php
@@ -0,0 +1,39 @@
+<?php namespace WP_GDPR\Classes;
+
+
+class Menu 
+{
+
+
+	public static function addAdminMenuPages()
+	{
+		
+		add_menu_page(
+            __( 'WP GDPR', 'wp_gdpr' ),
+            __( 'WP GDRP Consent', 'wp_gdpr' ),
+            static::managePermission(),
+            'ninja-wp-gdpr.php',
+            array( static::class, 'renderMortgage'),
+            '',
+            6
+        );
+	}
+
+
+
+	public static function managePermission()
+	{
+		return apply_filters('ninja_wp_gdpr_menu_manager_permission', 'manage_options');
+	}
+
+
+	public static function renderMortgage()
+	{
+		wp_enqueue_script('ninja_wp_gdpr_js', WP_GDPR_PLUGIN_DIR_URL.'public/js/ninja_wp_gdpr.js', array('jquery'), WP_GDPR_PLUGIN_DIR_VERSION, true );
+
+		include	WP_GDPR_PLUGIN_DIR_PATH.'views/admin_view.php';
+	}
+
+
+}
+
---
+++ wp_gdpr.php
@@ -0,0 +1,77 @@
+<?php
+
+defined("ABSPATH") or die;
+
+/*
+Plugin Name: WP GDPR Consent
+Description: The Best WP GDPR Consent Plugin for WordPress
+Version: 1.0.0
+Author: WPManageNinja
+Author URI: https://wpmanageninja.com
+Plugin URI: https://wpmanageninja.com/products/ninja-mortgage-calculator-plugin
+License: GPLv2 or later
+Text Domain: wp_gdpr
+Domain Path: /languages
+*/
+
+include "load.php";
+define("WP_GDPR_PLUGIN_DIR_URL", plugin_dir_url(__FILE__));
+define("WP_GDPR_PLUGIN_DIR_PATH", plugin_dir_path(__FILE__));
+define("WP_GDPR_PLUGIN_DIR_VERSION", plugin_dir_path(__FILE__));
+
+
+class NINJAWPGDPR
+{
+	public function boot()
+	{
+		$this->commonHooks();
+		$this->adminHooks();
+		$this->publicHooks();
+		$this->loadTextDomain();
+	}
+
+
+
+	public function commonHooks()
+	{
+		add_action('wp_enqueue_scripts', array($this, 'enqueueScripts') );
+
+	}
+	
+	public function adminHooks()
+	{
+	
+		add_action('admin_menu', array('WP_GDPR\Classes\Menu','addAdminMenuPages'));
+	}
+
+
+	public function publicHooks()
+	{
+		
+	}
+
+	public function enqueueScripts()
+	
+	{
+		wp_enqueue_style('ninja_wp_gdpr_css', WP_GDPR_PLUGIN_DIR_URL.'/public/css/styles.css');
+
+
+	}
+
+	public function loadTextDomain()
+	{
+		
+	}
+
+
+
+
+
+	
+}
+
+
+add_action('plugins_loaded', function(){
+	$ninjaWpGDPR = new NINJAWPGDPR();
+	$ninjaWpGDPR->boot(); 
+});
