9b9a6ef9d52402c293a078b2276eca858787729e
Ruhel <ruhel241@gmail.com>
2018-07-24 11:56:24 +0600

    added Select Theme, Color Customization,

--- src/js/components/AllConfig.vue
+++ src/js/components/AllConfig.vue
@@ -1,9 +1,18 @@
 <template>
 	<div class="wp_gdpr_all_config">
 		<div class="preview">
-			<div :class="{bannerBottom: bottom, bannerTop: top}">
-				<p>This website uses cookies to ensure you get the best experience on our website.</p>
-				<a href="#">Learn More</a> <el-button type="primary" round size="mini" class="gotItBtn">Got it</el-button>
+			<div :class="{
+						  bannerBottom: bottom, 
+						  bannerTop: top, 
+						  bannerLeft: left, 
+						  bannerRight: right,
+						  blackBgColor: bgBlackColor,
+						  blueBgColor: bgBlueColor,
+						  redBgColor: bgRedColor,
+						  purpleBgColor: bgPurpleColor
+						}">
+				<p>{{ message }}</p>
+				<a href="#">{{ policyLinkText }}</a> <el-button type="primary" round size="mini" class="gotItBtn">{{ dismissBtnText }}</el-button>
 			</div>
 		</div>
 		<div class="options">
@@ -11,30 +20,119 @@
 			  <el-collapse-item title="Select Theme" name="1">
 			    <ul>
 			    	<li>
-			    		<el-radio v-model="radio"  label="banner_bottom">Banner Bottom</el-radio>
+			    		<el-radio v-model="theme"  label="banner_bottom">Banner Bottom</el-radio>
 			    	</li>
 			    	<li>
-			    		<el-radio v-model="radio" label="banner_top">Banner Top</el-radio>
+			    		<el-radio v-model="theme" label="banner_top">Banner Top</el-radio>
 			    	</li>
 			    	<li>
-			    		<el-radio v-model="radio" label="3">Banner Top(Pushdown)</el-radio>
+			    		<el-radio v-model="theme" label="3">Banner Top(Pushdown)</el-radio>
 			    	</li>
 			    	<li>
-			    		<el-radio v-model="radio" label="4">Left</el-radio>
+			    		<el-radio v-model="theme" label="banner_left">Left</el-radio>
 			    	</li>
 			    	<li>
-			    		<el-radio v-model="radio" label="5">Right</el-radio>
+			    		<el-radio v-model="theme" label="banner_right">Right</el-radio>
 			    	</li>
 			    </ul>
 			  </el-collapse-item>
-			  <el-collapse-item title="Color Customization" name="2">
-			    <div>Operation feedback: enable the users to clearly perceive their operations by style updates and interactive effects;</div>
-			    <div>Visual feedback: reflect current state by updating or rearranging elements of the page.</div>
+			  <el-collapse-item title="Color Customization" name="2" class="color-customization">
+			  	<div class="bgColorList">
+			    	<p>Background Color</p>
+				    <ul>
+				    	<li>
+				    		<el-radio v-model="bg_color" label="black">Black</el-radio>
+				    	</li>
+				    	<li>
+				    		<el-radio v-model="bg_color" label="blue">Blue</el-radio>
+				    	</li>
+				    	<li>
+				    		<el-radio v-model="bg_color" label="red">Red</el-radio>
+				    	</li>
+				    	<li>
+				    		<el-radio v-model="bg_color" label="purple">Purple</el-radio>
+				    	</li>
+				    </ul>
+				</div>
+				<div class=textColorList>
+					<p>Text Color</p>
+				    <ul>
+				    	<li>
+				    		<el-radio v-model="text_color" label="black">Black</el-radio>
+				    	</li>
+				    	<li>
+				    		<el-radio v-model="text_color" label="blue">Blue</el-radio>
+				    	</li>
+				    	<li>
+				    		<el-radio v-model="text_color" label="red">Red</el-radio>
+				    	</li>
+				    	<li>
+				    		<el-radio v-model="text_color" label="purple">Purple</el-radio>
+				    	</li>
+				    </ul>
+				</div>
+				<div class=btnColorList>
+					<p>Button Background</p>
+				    <ul>
+				    	<li>
+				    		<el-radio v-model="btn_bg" label="black">Black</el-radio>
+				    	</li>
+				    	<li>
+				    		<el-radio v-model="btn_bg" label="blue">Blue</el-radio>
+				    	</li>
+				    	<li>
+				    		<el-radio v-model="btn_bg" label="red">Red</el-radio>
+				    	</li>
+				    	<li>
+				    		<el-radio v-model="btn_bg" label="purple">Purple</el-radio>
+				    	</li>
+				    </ul>
+				</div>
+				<div class="fntSize">
+					<p>Font Size</p>
+				    <ul>
+				    	<li>
+				    		<el-radio v-model="fnt_size" label="5px">5px</el-radio>
+				    	</li>
+				    	<li>
+				    		<el-radio v-model="fnt_size" label="10px">10px</el-radio>
+				    	</li>
+				    	<li>
+				    		<el-radio v-model="fnt_size" label="15px">15px</el-radio>
+				    	</li>
+				    	<li>
+				    		<el-radio v-model="fnt_size" label="20px">20px</el-radio>
+				    	</li>
+				    </ul>
+				</div>
 			  </el-collapse-item>
-			  <el-collapse-item title="Settings" name="3">
-			    <div>Simplify the process: keep operating process simple and intuitive;</div>
-			    <div>Definite and clear: enunciate your intentions clearly so that the users can quickly understand and make decisions;</div>
-			    <div>Easy to identify: the interface should be straightforward, which helps the users to identify and frees them from memorizing and recalling.</div>
+			  <el-collapse-item title="Custom Text" name="3">
+			  	<div class="cstm_text">
+				    <el-row :gutter="12" class="cstm_msg">
+				    	<el-col :span="24">
+				    		<label>Message</label>
+				    		<el-input type="textarea" v-model="message" :rows="3" placeholder="This website uses cookies to ensure you get the best experience on our website."></el-input>
+				    	</el-col>
+				    </el-row>
+				    <el-row :gutter="12">
+				    	<el-col :span="12">
+				    		<label>Dismiss button text</label>
+				    		<el-input v-model="dismissBtnText" placeholder="Got it!"></el-input>
+				    	</el-col>
+				    	<el-col :span="12">
+				    		<label>Policy link text</label>
+				    		<el-input v-model="policyLinkText" placeholder="Learn More"></el-input>
+				    	</el-col>
+				    </el-row>
+				</div>
+			  </el-collapse-item>
+			  <el-collapse-item title="Settings" name="4">
+			    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 			  </el-collapse-item>
 			</el-collapse>
 		</div>
@@ -46,21 +144,76 @@
 		data() {
 			return {
 				activeName: '',
+				message: 'This website uses cookies to ensure you get the best experience on our website.',
+				policyLinkText: 'Learn More',
+				dismissBtnText: 'Got it!',
 				bottom: true,
 				top: false,
-				radio: 'banner_bottom'
+				left: false,
+				right: false,
+				theme: 'banner_bottom',
+				bg_color: 'black',
+				text_color: 'black',
+				bgBlackColor: false,
+				bgBlueColor: false,
+				bgRedColor: false,
+				bgPurpleColor: false,
+				btn_bg: 'black',
+				fnt_size: '5px'
 			}
 		},
 		watch: {
-			radio() {
-				console.log(this.radio);
-				if( this.radio == 'banner_bottom') {
+			theme() {
+				console.log(this.theme);
+				if( this.theme == 'banner_bottom' ) {
 					this.bottom = true;
 					this.top = false;
+					this.left = false;
+					this.right = false;
 				}
-				else if( this.radio == 'banner_top' ) {
+				else if( this.theme == 'banner_top' ) {
 					this.top = true;
 					this.bottom = false;
+					this.left = false;
+					this.right = false;
+				}
+				else if( this.theme == 'banner_left' ) {
+					this.left = true;
+					this.right = false;
+					this.top = false;
+					this.bottom = false;
+				}
+				else if( this.theme == 'banner_right' ) {
+					this.right = true;
+					this.top = false;
+					this.bottom = false;
+					this.left = false;
+				}
+			},
+			bg_color() {
+				if( this.bg_color == 'black' ) {
+					this.bgBlackColor = true;
+					this.bgBlueColor = false;
+					this.bgRedColor = false;
+					this.bgPurpleColor = false;
+				}
+				else if( this.bg_color == 'blue' ) {
+					this.bgBlueColor = true;
+					this.bgBlackColor = false;
+					this.bgRedColor = false;
+					this.bgPurpleColor = false;
+				}
+				else if( this.bg_color == 'red' ) {
+					this.bgRedColor = true;
+					this.bgBlackColor = false;
+					this.bgBlueColor = false;
+					this.bgPurpleColor = false;
+				}
+				else if( this.bg_color == 'purple' ) {
+					this.bgPurpleColor = true;
+					this.bgRedColor = false;
+					this.bgBlackColor = false;
+					this.bgBlueColor = false;
 				}
 			}
 		}
@@ -87,8 +240,21 @@
 					padding: 0 0px 0px 13px;
 				}
 			}
-
 		}
+
+
+		.color-customization {
+			.el-collapse-item__content{
+				// padding-left: 13px;
+			  p {
+		   	    	margin-left: 13px;
+		   		}
+			}
+		 }
+
+
+
+
 
 		.el-collapse-item__header {
 			background: #83C6CF;
@@ -108,7 +274,7 @@
 
 		.bannerBottom {
 		    bottom: 0px;
-		    background: purple;
+		    background: black;
 		    color: white;
 		    position: relative;
 		    width: 100%;
@@ -132,7 +298,7 @@
 		}
 
 		.bannerTop {
-			background: purple;
+			background: black;
 		    color: white;
 		    width: 100%;
 		    margin: 0;
@@ -145,12 +311,103 @@
 			a {
 			 	color:wheat;
 			}
+			.gotItBtn {
+				float: right;
+				margin-top: 10px;
+				margin-right: 8px;
+			}
 		}
 
-		.gotItBtn {
+		.bannerLeft {
+			background: black;
+		    color: white;
+		    max-width: 24em;
+		    padding: 3em;
+		    margin-top: 291px;
+		    margin-left: 15px;
+		    border-radius: 10px;
+		    p {
+		    	padding: 0;
+			    margin: 0;
+			    font-size: 16px;
+		    }
+		    a {
+		    	color: wheat;
+		    }
+
+			.gotItBtn {
+		    	float: right;
+			    margin-top: 6px;
+		    }
+
+		}
+
+		.bannerRight {
+			background: black;
+		    color: white;
+		    max-width: 24em;
+		    padding: 3em;
+		    margin-top: 276px;
+		    float: right;
+		    margin-right: 15px;
+		    border-radius: 10px;
+			p {
+		    	padding: 0;
+			    margin: 0;
+			    font-size: 16px;
+		    }
+		    a {
+		    	color: wheat;
+		    }
+
+			.gotItBtn {
+		    	float: right;
+			    margin-top: 6px;
+		    }
+		}
+
+		.bgColorList {
+			width: 50%;
+			float: left;
+		}
+
+		.textColorList {
+			width: 50%;
 			float: right;
-			margin-top: 10px;
-			margin-right: 8px;
+		}
+
+		.btnColorList {
+			width: 50%;
+			float: left;
+		}
+
+		.fntSize {
+			width: 50%;
+			float: right;
+		}
+
+		.cstm_text {
+			padding: 20px;
+			.cstm_msg {
+				padding-bottom: 10px;
+			}
+		}
+
+
+		.blackBgColor {
+			background: black;
+		}
+
+		.blueBgColor {
+			background: blue;
+		}
+
+		.redBgColor {
+			background: red;
+		}
+
+		.purpleBgColor {
+			background: purple;
 		}
 	}
 </style>
